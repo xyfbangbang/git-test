19d099869a79983fc7015d0b4fb9960cd26f953a
xyf <2396802220@qq.com>
2022-04-15 19:59:08 +0800

    master commit2

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -9,5 +9,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("HELLLLLLLL2!!");
         System.out.println("HELLLLLLLL22!!");
+        System.out.println("master!!");
     }
 }
